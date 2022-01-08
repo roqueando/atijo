a07960524bec49dc8223f4af5da6b590ffaa2aa5
Vitor Roque <vitor.roquep@gmail.com>
2022-01-08 18:59:38 -0300

    fix

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,9 @@
-#define CROW_MAIN
 #include <iostream>
-#include <deps/Crow/include/crow.h>
+#include <evpp/http/http_server.h>
 
 int main()
 {
     //fmt::print("Opa amigao {}", "BELE");
+    std::cout << "Opa amigao";
     return 0;
 }
