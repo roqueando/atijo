ead2f6cd9c41064f3285e60e4e04cf1024ba8ea3
Vitor Roque <vitor.roquep@gmail.com>
2022-01-08 21:33:05 -0300

    fix

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <evpp/http/http_server.h>
+#include <httplib.h>
 
 int main()
 {
-    //fmt::print("Opa amigao {}", "BELE");
-    std::cout << "Opa amigao";
-    return 0;
+    httplib::Server server;
+    // separate that by handlers
+    server.Get("/", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("hello guys", "text/plain");
+    });
+
+    std::cout << "Serving application on port 8000";
+    server.listen("0.0.0.0", 8000);
 }
